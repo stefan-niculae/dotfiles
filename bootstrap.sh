4b6a779efa4f18e8356ea154b89f1a080d826450
Stefan Niculae <stefan.niculae@intel.com>
2015-09-14 10:36:35 +0300

    Back to this

--- bootstrap.sh
+++ bootstrap.sh
@@ -2,7 +2,7 @@
 
 dir=~/dotfiles                    # dotfiles directory
 olddir=~/dotfiles_old             # old dotfiles backup directory
-files=".bashrc .vimrc .vim"        # list of files/folders to symlink in homedir
+files=".bashrc .vimrc .vim .gitconfig" # list of files/folders to symlink in homedir
 
 ##########
 
@@ -16,7 +16,7 @@ echo "Changing to the $dir directory"
 cd $dir
 echo "...done"
 
-# move any existing dotfiles in homedir to dotfiles_old directory, then create symlinks 
+# move any existing dotfiles in homedir to dotfiles_old directory, then create symlinks
 for file in $files; do
     echo "Moving any existing dotfiles from ~ to $olddir"
     mv ~/$file ~/dotfiles_old/
